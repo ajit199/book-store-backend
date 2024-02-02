fb1d824babad97af3749b60f64a96154462d504c
ajit@199 <ajitca.1999@gmail.com>
2024-02-02 07:50:15 +0530

    send mail to authors completed.

--- controllers/books/buyBook.js
+++ controllers/books/buyBook.js
@@ -1,6 +1,5 @@
 const { Book, PurchaseHistory, Revenue } = require("../../models");
-const mongoose = require("mongoose");
-
+const sendMail = require("../../utils/sendMail");
 async function buyBook(req, res, next) {
   try {
     const { _id } = req.user;
@@ -9,12 +8,6 @@ async function buyBook(req, res, next) {
     const book = await Book.findOne({ _id: bookId });
 
     const purchaseId = await generateNextPurchaseId();
-
-    const transactionOptions = {
-      readPreference: "primary",
-      readConcern: { level: "local" },
-      writeConcern: { w: "majority" },
-    };
 
     const history = new PurchaseHistory({
       bookId,
@@ -39,6 +32,8 @@ async function buyBook(req, res, next) {
     }));
 
     await Revenue.insertMany(authorsReveue);
+
+    sendMailToAuthors(book);
 
     res.json({
       data: book,
@@ -72,5 +67,115 @@ async function generateNextPurchaseId() {
     return purchaseId;
   } catch (error) {
     console.log("error", error);
+  }
+}
+
+async function sendMailToAuthors(book) {
+  try {
+    const aggregatePipeline = [
+      {
+        $match: {
+          userId: {
+            $in: book?.authors,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$userId",
+          totalRevenue: {
+            $sum: "$revenue",
+          },
+          currentYearRevenue: {
+            $sum: {
+              $cond: {
+                if: {
+                  $eq: [
+                    {
+                      $year: "$createdAt",
+                    },
+                    {
+                      $year: new Date(),
+                    },
+                  ],
+                },
+                then: "$revenue",
+                else: 0,
+              },
+            },
+          },
+          currentMonthRevenue: {
+            $sum: {
+              $cond: {
+                if: {
+                  $and: [
+                    {
+                      $eq: [
+                        {
+                          $year: "$createdAt",
+                        },
+                        {
+                          $year: new Date(),
+                        },
+                      ],
+                    },
+                    {
+                      $eq: [
+                        {
+                          $month: "$createdAt",
+                        },
+                        {
+                          $month: new Date(),
+                        },
+                      ],
+                    },
+                  ],
+                },
+                then: "$revenue",
+                else: 0,
+              },
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          totalRevenue: 1,
+          currentYearRevenue: 1,
+          currentMonthRevenue: 1,
+          userEmail: {
+            $first: "$user.email",
+          },
+        },
+      },
+    ];
+
+    const authorsReveue = await Revenue.aggregate(aggregatePipeline);
+
+    if (authorsReveue.length) {
+      for (const revenue of authorsReveue) {
+        let body = `
+        Dear Author, Your book ${book?.title} has been purchased. <br><br>
+        
+        Below is your current revenue stauts: <br>
+        
+        <p><b>Current Month Revenue :</b> ${revenue.currentMonthRevenue}</p>
+        <p><b>Current Year Revenue :</b> ${revenue.currentYearRevenue}</p>
+        <p><b>Total Revenue :</b> ${revenue.totalRevenue}</p>
+        `;
+        sendMail(revenue.userEmail, body, "Book Purchase Notification");
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
